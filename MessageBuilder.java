96cbb77013106584dc3987aa8f2c77df12336f57
Hapyl <Hapyl@users.noreply.github.com>
2019-10-15 05:12:55 +0600

    Update MessageBuilder.java

--- MessageBuilder.java
+++ MessageBuilder.java
@@ -4,25 +4,29 @@ import org.bukkit.entity.Player;
 
 public class MessageBuilder {
 
-    private TextComponent message;
+        private TextComponent message;
 
     public MessageBuilder(String message) {
-        this.message = new TextComponent(ChatColor.translateAlternateColorCodes('&', message));
+        this.message = new TextComponent(f(message));
     }
 
     public MessageBuilder setClickEvent(ClickEvent.Action a, String value) {
-        this.message.setClickEvent(new ClickEvent(a, value));
+        this.message.setClickEvent(new ClickEvent(a, f(value)));
         return this;
     }
 
-    public MessageBuilder setHoverEvent(HoverEvent.Action a, String value){
-        this.message.setHoverEvent(new HoverEvent(a, new ComponentBuilder(value).create()));
+    public MessageBuilder setHoverEvent(HoverEvent.Action a, String value) {
+        this.message.setHoverEvent(new HoverEvent(a, new ComponentBuilder(f(value)).create()));
         return this;
     }
 
     public MessageBuilder send(Player player) {
         player.spigot().sendMessage(this.message);
         return this;
+    }
+
+    private String f(String s){
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
     
 }
