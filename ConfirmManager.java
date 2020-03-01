6880a59fbd5a775dce9cf81538e4a816a8a89d71
Hapyl <Hapyl@users.noreply.github.com>
2020-03-01 13:14:25 +0600

    Added new features.

--- ConfirmManager.java
+++ ConfirmManager.java
@@ -1,5 +1,6 @@
 package YOUR.PACKAGE.GOES.HERE
 
+
 import net.md_5.bungee.api.chat.*;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -25,23 +26,33 @@ public final class ConfirmManager implements Listener {
     private long startedAt;
     private long expireLimitMillis;
 
+    // Format:
+    // { message , hover message }
+    private String[] confirmMessage = {"&a&l[CONFIRM]", "&7Click to confirm!"};
+    private String[] cancelMessage = {"&c&l[CANCEL]", "&7Click to cancel!"};
+
     private Consumer<? super Player> confirmAction;
     private Consumer<? super Player> cancelAction;
 
     public final String CONFIRM_COMMAND = "+confirm";
     public final String CANCEL_COMMAND = "+cancel";
 
-    /**
+     /**
      * [!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!]
      * [!] Don't forget to register events for this class [!]
-     * [!] [!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!]
+     * [!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!][!]
+     *
+     * Updates:
+     *  v 0.2:
+     *      - You can now change confirm and cancel messages by using '.setConfirmMessage' and 'setCancelMessage'
+     *      - [!] Builder now require '.ask()' at the end to send a message to player.
      *
      * Constructor for confirmation. (v 0.1)
      *      Usage Examples:
      *
-     *          new ConfirmManager(<player>, "&aConfrimation message that supports color cosed!", 0, p -> p.sendMessage("You just accepted it!"), p -> p.sendMessage("You just cancelled it!"));
-     *          new ConfirmManager(<player>, "&aClick CANCEL fast it'll expire in 1 second!", 1, p -> p.sendMessage("I said CANCEL!"), p -> p.sendMessage("Good Job!"));
-     *          new ConfirmManager(<player>, "&eDo you want to get Creative more?", 0, p -> {p.sendMessage("Enjoy."); player.setGameMode(GameMode.CREATIVE);}, p -> {p.sendMessage("k"); player.setGameMode(GameMode.SURVIVAL);});
+     *          new ConfirmManager(<player>, "&aConfrimation message that supports color cosed!", 0, p -> p.sendMessage("You just accepted it!"), p -> p.sendMessage("You just cancelled it!")).ask();
+     *          new ConfirmManager(<player>, "&aClick CANCEL fast it'll expire in 1 second!", 1, p -> p.sendMessage("I said CANCEL!"), p -> p.sendMessage("Good Job!")).ask();
+     *          new ConfirmManager(<player>, "&eDo you want to get Creative more?", 0, p -> {p.sendMessage("Enjoy."); player.setGameMode(GameMode.CREATIVE);}, p -> {p.sendMessage("k"); player.setGameMode(GameMode.SURVIVAL);}).ask();
      *
      *      Extra Info:
      *          - Leaving the server cancels confirmation.
@@ -58,7 +69,7 @@ public final class ConfirmManager implements Listener {
      * @param cancelAction - Consumer of <player> for cancel action.
      *                     
      * Todo:
-     *  Custom CONFIRM and CANCEL messages.
+     *  [X] Custom CONFIRM and CANCEL messages.
      *  Multiple confirmations (id's).
      *
      */
@@ -83,8 +94,36 @@ public final class ConfirmManager implements Listener {
         this.cancelAction = cancelAction;
 
         confirmStorage.put(player, this);
-        askConfirmation();
 
+    }
+
+    /**
+     * Used to change confirmation message (Colorized with '&')
+     *
+     * @param message      - Confirmation message.
+     * @param hoverMessage - Hover message. Keep empty for no hover effect.
+     */
+    public ConfirmManager setConfirmMessage(String message, String hoverMessage) {
+        this.confirmMessage = new String[]{message, hoverMessage};
+        return this;
+    }
+
+    /**
+     * Used to change cancel message (Colorized with '&')
+     *
+     * @param message      - Cancel message.
+     * @param hoverMessage - Hover message. Keep empty for no hover effect.
+     */
+    public ConfirmManager setCancelMessage(String message, String hoverMessage) {
+        this.cancelMessage = new String[]{message, hoverMessage};
+        return this;
+    }
+
+    /**
+     * Used to send player message about confirmation.
+     */
+    public void ask() {
+        this.askConfirmation();
     }
 
     private boolean checkExpiration() {
@@ -98,17 +137,21 @@ public final class ConfirmManager implements Listener {
 
     private void askConfirmation() {
 
-        final BaseComponent[] accept = new ComponentBuilder(colorize("&a&l[CONFIRM]"))
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(colorize("&7Click to confirm!")).create()))
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/" + this.CONFIRM_COMMAND)).create();
+        final ComponentBuilder accept = new ComponentBuilder(colorize(this.confirmMessage[0]))
+                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/" + this.CONFIRM_COMMAND));
 
-        final BaseComponent[] cancel = new ComponentBuilder(colorize("&c&l[CANCEL]"))
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(colorize("&7Click to cancel!")).create()))
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/" + this.CANCEL_COMMAND)).create();
+        if (!this.confirmMessage[1].isEmpty())
+            accept.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(colorize(this.confirmMessage[1])).create()));
+
+        final ComponentBuilder cancel = new ComponentBuilder(colorize(this.cancelMessage[0]))
+                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/" + this.CANCEL_COMMAND));
+
+        if (!this.cancelMessage[1].isEmpty())
+            cancel.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(colorize(this.cancelMessage[1])).create()));
 
         this.player.sendMessage(this.message);
-        this.player.spigot().sendMessage(accept);
-        this.player.spigot().sendMessage(cancel);
+        this.player.spigot().sendMessage(accept.create());
+        this.player.spigot().sendMessage(cancel.create());
 
     }
 
